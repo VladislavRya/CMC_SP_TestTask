142b3570b109f4acc016f7720f2c2e2d7f825e59
VladislavRya <ryabykin.vlad@mail.ru>
2022-04-11 22:18:47 +0300

    Added 1 code example for manual testing
    Added 1 code example for manual testing in the folder "Code Examples Manual Testing"

---
+++ Code Examples Manual Testing/ManualTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ManualAnalyzersCheck
+{
+    class VeryLongClassName
+    {
+        int veryLongInt;
+        int shortInt;
+        int veryLongInt2, shortInt2, veryLongInt3;
+        string veryLongString = "Hello", shortStr;
+
+        public string VeryLongString
+        {
+            get { return veryLongString; }
+            set { veryLongString = value; }
+        }
+            
+        public int ShortInt
+        {
+            get { return shortInt; }
+        }
+
+        public void Func() { }
+        public void VeryLongFunc() { }
+
+    }
+    
+    class ShortClass
+    {
+        string veryLongString;
+
+        public string VeryLongString
+        {
+            get { return veryLongString; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int veryLongMainInt;
+            int shortInt;
+            int veryLongMainInt2, shortInt2, veryLongMainInt3, shortInt3;
+        }
+    }
+}
